3c8b8be61928cbcc58eef6505fa5b7f2743ba0e7
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-02-03 15:12:32 -0300

    Create Endereco.h

---
+++ Endereco.h
@@ -0,0 +1,18 @@
+#ifndef ENDERECO_H
+#define ENDERECO_H
+#include <string>
+
+using namespace std;
+class Endereco
+{
+    public:
+        Endereco();
+        string rua, bairro, cidade, cep;
+        int numero;
+
+    protected:
+
+    private:
+};
+
+#endif // ENDERECO_H
