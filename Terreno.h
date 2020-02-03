74b83835907195f635f73ce0d5ee82e5d2247d72
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-02-03 15:11:56 -0300

    Create Terreno.h

---
+++ Terreno.h
@@ -0,0 +1,17 @@
+#ifndef TERRENO_H
+#define TERRENO_H
+#include <string>
+#include "Imovel.h"
+
+class Terreno : public Imovel
+{
+    public:
+        Terreno();
+        double area;
+
+    protected:
+
+    private:
+};
+
+#endif // TERRENO_H
