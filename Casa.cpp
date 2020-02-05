3e58a9d8fb86e3ef6670902e2fafc2791926d521
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-02-05 14:37:02 -0300

    Update Casa.cpp

--- Casa.cpp
+++ Casa.cpp
@@ -9,14 +9,16 @@ Casa::Casa()
     this->areaConst = 0;
     this->areaTer = 0;
     this->tipoImovel = 2;
+    disponivel = 0;
 }
 
-Casa::Casa(int NUMPAVIMEN, int NUMQUARTOS, double AREACONST, double AREATER){
+Casa::Casa(int NUMPAVIMEN, int NUMQUARTOS, double AREACONST, double AREATER, Endereco ENDE){
     this->numPavimen = NUMPAVIMEN;
     this->numQuartos = NUMQUARTOS;
     this->areaConst = AREACONST;
     this->areaTer = AREATER;
     this->tipoImovel = 2;
+    this->endereco = ENDE;
+    disponivel = 0;
 }
-
 
