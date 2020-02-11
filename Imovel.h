26edd4ee14cc5003df0aac7006c55ad4ebfd03ac
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-02-10 21:00:46 -0300

    Update Imovel.h

--- Imovel.h
+++ Imovel.h
@@ -1,41 +1,42 @@
-#ifndef IMOVEL_H	
-#define IMOVEL_H	
-#include "Endereco.h"	
-#include <string>	
+#ifndef IMOVEL_H
+#define IMOVEL_H
+#include "Endereco.h"
+#include <string>
 
-using namespace std;	
-class Imovel	
+using namespace std;
+class Imovel
 {
-    public:	{
-        Imovel();	    
+    public:
+        Imovel();
 
-        bool getAouV();	
-        double getValor();	
-        Endereco getEndereco();	
-        int getTipo();	    
-        string getDescricao();	
-        string getTitulo();	
+        bool getAouV();
+        double getValor();
+        Endereco getEndereco();
+        int getTipo();
+        string getDescricao();
+        string getTitulo();
 
-        void setAouv(bool Aouv);	
-        void setValor(double val);	    
-        void setEndereco(Endereco ende);	
-        void setTipo(int tip);	
+        void setAouV(bool Aouv);
+        void setValor(double val);
+        void setEndereco(Endereco ende);
+        void setTipo(int tip);
         void setDescricao(string desc);
-        void setTitulo(string titu);	
-        
-        bool disponivel
+        void setTitulo(string titu);
+
+        bool disponivel;
 
 
     protected:
-        int tipoImovel;	
-        Endereco endereco;	
-        bool aouv;	
-        double valor;	
-        string descricao;	  
-        string titulo;	
+        int tipoImovel;
+        Endereco endereco;
+        bool aouv;
+        double valor;
+        string descricao;
+        string titulo;
 
-    private:	
-       
+    private:
+
 };
 
 #endif // IMOVEL_H
+
